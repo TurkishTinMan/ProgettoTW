71c65ec6048c40c3c39b13ea86ae5a0fc78bdfe4
djammaj <lucifer14@hotmail.com>
2016-10-13 17:53:58 +0200

    Add files via upload

---
+++ PHP/loaderDocument_1.php
@@ -0,0 +1,14 @@
+<?php
+include('simple_html_dom.php');
+
+$document = array();
+
+$doc_body = new simple_html_dom();
+$doc_body->load_file($_POST['localUrl']);
+
+$doc_title = $doc_body->find('title',0)->innertext();
+
+$document[$doc_title] = $doc_body;
+
+echo json_encode($document);
+?>
