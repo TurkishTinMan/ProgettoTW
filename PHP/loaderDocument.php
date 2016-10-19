ef2a96442f1afa62a2bd023d3d3a37953d132967
Riccardo Salladini <riccardo.salladini@gmail.com>
2016-10-19 17:48:41 +0200

    css+Script

--- PHP/loaderDocument.php
+++ PHP/loaderDocument.php
@@ -7,16 +7,41 @@ $document = array();
 $doc = new simple_html_dom();
 $doc -> load_file($_POST['localUrl']);
 
-$doc_body = $doc -> find('body', 0);
+$doc_body = $doc;
 
 $doc_title = $doc -> find('title', 0) -> innertext;
 
 $tags = $doc_body -> find('img');
-    foreach ($tags as $tag) {
-        $old_src = $tag -> src;
+foreach ($tags as $tag) {
+    $old_src = $tag -> src;
+    $new_src_url = './Dataset/project-files/dataset/'.$old_src;
+    $tag -> src = $new_src_url;
+}
+
+$scripts = $doc_body -> find('script');
+foreach ($scripts as $script) {
+    $old_src = $script -> src;
+    if (0 !== strpos($old_src, 'http')) {
         $new_src_url = './Dataset/project-files/dataset/'.$old_src;
-        $tag -> src = $new_src_url;
+        $script -> src = $new_src_url;
     }
+}
+
+
+$styles = $doc_body -> find('link');
+foreach ($styles as $style) {
+    $old_src = $style -> src;
+    if (0 !== strpos($old_src, 'http')) {
+        $new_src_url = './Dataset/project-files/dataset/'.$old_src;
+        $style -> src = $new_src_url;
+    }
+}
+
+$headers = $doc_body -> find('header');
+foreach($headers as $header){
+    $header->innertext = "cacca";
+}
+
 
 $document[$doc_title] = $doc_body -> innertext;
 
